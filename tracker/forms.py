b268313e2185093284d20416cda962cb3375d614
aqibcs <csaqibshah@gmail.com>
2024-11-19 05:47:07 +0000

    Add forms for password reset request

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Expense
 from django.core.exceptions import ValidationError
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 
 class UserRegistrationForm(forms.ModelForm):
@@ -23,7 +24,6 @@ class UserRegistrationForm(forms.ModelForm):
                 'Username already exists. Please choose a different one')
         return username
 
-
     def clean_password2(self):
         """
         Ensure that both password fields match.
@@ -42,3 +42,48 @@ class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=100, required=True)
     password = forms.CharField(widget=forms.PasswordInput, required=True)
 
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not User.objects.filter(email=email).exists():
+            raise ValidationError("This email is not registered.")
+        return email
+
+
+class SetNewpasswordForm(forms.Form):
+    new_password = forms.CharField(widget=forms.PasswordInput,
+                                   label="New Password")
+    confirm_password = forms.CharField(widget=forms.PasswordInput,
+                                       label="Confirm New Password")
+
+    def __init__(self, *args, **kwargs):
+        self.uidb64 = kwargs.pop('uidb64', None)
+        self.token = kwargs.pop('token', None)
+        super().__init__(*args, **kwargs)
+
+    def save(self):
+        cleaned_data = super.clean()
+        new_password = cleaned_data.get('new_password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if new_password != confirm_password:
+            raise ValidationError("Password don't match.")
+        return cleaned_data
+
+    def save(self):
+        try:
+            uid = urlsafe_base64_decode(self.uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, User.DoesNotExist):
+            raise ValidationError("Invalid user.")
+
+        # Verify token
+        if not default_token_generator.check_token(user, self.token):
+            raise ValidationError("Invalid or expired token.")
+
+        user.set_password(self.cleaned_data['new_password'])
+        user.save()
+        return user
